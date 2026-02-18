82ad27a8adc9b9cdf330bb04a6ebe15dba8f4acc
Anton Zlygostev <zlygostev@hotmail.com>
2026-02-18 17:19:27 +0700

    Fixing missing role for the sakila example

--- examples/sakila/prepare.sql
+++ examples/sakila/prepare.sql
@@ -1,4 +1,14 @@
 \i '0-postgres-sakila-setup.sql'
 \i '1-postgres-sakila-schema.sql'
 \i '2-postgres-sakila-insert-data.sql'
+
+-- Keep upstream scripts vanilla; only provide missing role expected by upstream user script.
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'sakila') THEN
+        CREATE ROLE sakila;
+    END IF;
+END
+$$;
+
 \i '3-postgres-sakila-user.sql'
