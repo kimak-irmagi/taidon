a330c65ccc478b1d170b6edfcc31c86faabeff21
Anton Zlygostev <zlygostev@hotmail.com>
2026-02-25 18:50:06 +0700

    Fixing P2 with free bytes reporting on Windows

--- backend/local-engine-go/internal/prepare/filesystem_stats_windows.go
+++ backend/local-engine-go/internal/prepare/filesystem_stats_windows.go
@@ -4,6 +4,8 @@ package prepare
 
 import "golang.org/x/sys/windows"
 
+var getDiskFreeSpaceEx = windows.GetDiskFreeSpaceEx
+
 func filesystemStats(path string) (int64, int64, error) {
 	statsPath, err := resolveFilesystemStatPath(path)
 	if err != nil {
@@ -16,8 +18,8 @@ func filesystemStats(path string) (int64, int64, error) {
 	var freeBytesAvailable uint64
 	var totalBytes uint64
 	var totalFreeBytes uint64
-	if err := windows.GetDiskFreeSpaceEx(ptr, &freeBytesAvailable, &totalBytes, &totalFreeBytes); err != nil {
+	if err := getDiskFreeSpaceEx(ptr, &freeBytesAvailable, &totalBytes, &totalFreeBytes); err != nil {
 		return 0, 0, err
 	}
-	return clampUint64ToInt64(totalBytes), clampUint64ToInt64(totalFreeBytes), nil
+	return clampUint64ToInt64(totalBytes), clampUint64ToInt64(freeBytesAvailable), nil
 }
